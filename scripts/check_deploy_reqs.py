ea070468b7c7c263ab67a69c23d9cd1ce6837997
didaktio <j@didakt.io>
2023-06-15 20:42:32 +0100

    ci: conditionally deploy repository components

---
+++ scripts/check_deploy_reqs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import subprocess
+from fnmatch import fnmatch
+
+api_include = ["laimpost/*", "laimpostapp/*", "requirements.txt", "manage.py", "*.py"]
+app_include = [
+    "laimpost.com/*",
+    "*.ts",
+    "*.tsx",
+    "*.js",
+    "*.json",
+    "*.css",
+    "*.scss",
+    "*.png",
+]
+files = (
+    subprocess.check_output(["git", "diff", "--name-only", "--cached"])
+    .decode()
+    .splitlines()
+)
+cicd_changed = any(fnmatch(f, ".github/workflows/*") for f in files)
+api_changed = cicd_changed or any(fnmatch(f, p) for p in api_include for f in files)
+app_changed = cicd_changed or any(fnmatch(f, p) for p in app_include for f in files)
+with open("component_changes.txt", "w") as file:
+    file.write(
+        f"API_CHANGED={api_changed}\nAPP_CHANGED={app_changed}\nCICD_CHANGED={cicd_changed}\n"
+    )
