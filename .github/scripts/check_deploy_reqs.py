f7731ab2e0012b41b8ba1e93dcee7a6e4569143f
didaktio <j@didakt.io>
2023-06-17 16:40:16 +0100

    chore: refine deployment check script [ci deploy-all]

--- .github/scripts/check_deploy_reqs.py
+++ .github/scripts/check_deploy_reqs.py
@@ -23,13 +23,15 @@ app_include = [
 ]
 
 with open(commit_info_file_name, "r") as file:
-    files = file.read().splitlines()
-    cicd_changed = any(fnmatch(f, ".github/workflows/*") for f in files)
-    api_changed = any(fnmatch(f, p) for p in api_include for f in files)
-    app_changed = any(fnmatch(f, p) for p in app_include for f in files)
-    changes = f"API={api_changed}\nAPP={app_changed}\nCICD={cicd_changed}\n"
-    if "[ci deploy-all]" in files[0]:
-        changes += "\nFORCE=True\n"
+    commit_message, *files = file.read().splitlines()
+    changes = ""
+    if "[ci deploy-all]" in commit_message:
+        changes = "FORCE=True\n"
+    else:
+        api_changed = any(fnmatch(f, p) for p in api_include for f in files)
+        app_changed = any(fnmatch(f, p) for p in app_include for f in files)
+        cicd_changed = any(fnmatch(f, ".github/workflows/*") for f in files)
+        changes = f"API={api_changed}\nAPP={app_changed}\nCICD={cicd_changed}\n"
 
     print(changes)
 
