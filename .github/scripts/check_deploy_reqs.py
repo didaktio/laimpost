65a0d2dadd6591ca506cf5960add6970a7d0e5f1
didaktio <j@didakt.io>
2023-06-27 15:18:02 +0100

    perf: re-add middleware; handle min file changes in deploy reqs checker

--- .github/scripts/check_deploy_reqs.py
+++ .github/scripts/check_deploy_reqs.py
@@ -23,15 +23,18 @@ app_include = [
 ]
 
 with open(commit_info_file_name, "r") as file:
-    commit_message, *files = file.read().splitlines()
     changes = ""
-    if "[ci deploy-all]" in commit_message:
-        changes = "FORCE=True\n"
-    else:
-        api_changed = any(fnmatch(f, p) for p in api_include for f in files)
-        app_changed = any(fnmatch(f, p) for p in app_include for f in files)
-        cicd_changed = any(fnmatch(f, ".github/workflows/*") for f in files)
-        changes = f"API={api_changed}\nAPP={app_changed}\nCICD={cicd_changed}\n"
+    try:
+        commit_message, *files = file.read().splitlines()
+        if "[ci deploy-all]" in commit_message:
+            changes = "FORCE=True\n"
+        else:
+            api_changed = any(fnmatch(f, p) for p in api_include for f in files)
+            app_changed = any(fnmatch(f, p) for p in app_include for f in files)
+            cicd_changed = any(fnmatch(f, ".github/workflows/*") for f in files)
+            changes = f"API={api_changed}\nAPP={app_changed}\nCICD={cicd_changed}\n"
+    except ValueError:
+        changes = "FORCE=False\n"
 
     print(changes)
 
